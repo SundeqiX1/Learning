91a18d015e126c98f72a4e20e8c0ff5114b44eee
SundeqiX1 <qiyu7293@gmail.com>
2021-11-01 16:48:59 +0800

    1101

--- 操作符详解/操作符详解/test10_22.c
+++ 操作符详解/操作符详解/test10_22.c
@@ -116,11 +116,152 @@
 //	return 0;
 //}
 
-int main()
+//int main()
+//{
+//	short s = 0;
+//	int a = 10;
+//	printf("%d\n", sizeof(s = a + 5));
+//	printf("%d\n", s); 
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 11;
+//	a = a | (1 << 2);
+//	printf("%d\n", a);
+//	a =a &(~(1 << 2));
+//	printf("%d\n", a);
+//	//00000000000000000000000000001011
+//	//
+//	//int a = 0;
+//	//~按位取反
+//	//00000000000000000000000000000000
+//	//11111111111111111111111111111111补码
+//	//11111111111111111111111111111110反码
+//	//10000000000000000000000000000001原码
+//	//-1
+//	//printf("%d\n", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	//printf("%d", ++a);//前置++，先++后使用
+//	printf("%d", a++);//后置++，先使用后++
+//	return 0;
+//}
+
+
+//void test1(int arr[])
+//{
+//	printf("%d\n", sizeof(arr));//指针的大小
+//}
+//
+//void test2(char ch[])
+//{
+//	printf("%d\n", sizeof(ch));//指针的大小
+//}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	char ch[10] = { 0 };
+//	printf("%d\n", sizeof(arr));
+//	printf("%d\n", sizeof(ch));
+//	test1(arr);
+//	test2(ch);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	int c = a && b;
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0, a = 1, b = 2, c = 3, d = 4;
+//	//i = a++&&++b&&d++;
+//	i=a++||++b||d++;
+//	printf("a=%d\nb=%d\nc=%d\nd=%d\n", a, b, c, d);
+//	return 0;
+//}
+
+//学生
+//创建一个结构体类型--struct Stu
+struct Stu
 {
-	short s = 0;
-	int a = 10;
-	printf("%d\n", sizeof(s = a + 5));
-	printf("%d\n", s); 
-	return 0;
-}
+	char name[20];
+	int age;
+	char id[20];
+};
+//int main()
+//{
+//	//使用struct Stu这个结构体类型创建了一个学生对象s1，并初始化
+//	struct Stu s1 = { "孙德奇",23,"20211101" };
+//	struct Stu* ps = &s1;
+//	printf("%s\n", ps->name);
+//
+//	//printf("%s\n", (*ps).name);
+//	//printf("%d\n", (*ps).age);
+//
+//	//printf("%s\n", s1.name);
+//	//printf("%d\n", s1.age);
+//	//printf("%s\n", s1.id);
+//	//结构体变量.成员名
+//	return 0;
+//}
+
+//int main()
+//{
+//	char a = 3;
+//	//00000000000000000000000000000011
+//	//00000011--a
+//	char b = 127;
+//	//00000000000000000000000001111111
+//	//01111111--b
+//	//a+b
+//	//整型提升
+//	//00000000000000000000000000000011
+//	//00000000000000000000000001111111
+//	//00000000000000000000000010000010
+//	//
+//	char c = a + b;
+//	//10000010--c
+//	//整型提升
+//	//11111111111111111111111110000010--补码
+//	//11111111111111111111111110000001--反码
+//	//10000000000000000000000001111110--原码
+//	//-126
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+//整型提升实例：
+//int main()
+//{
+//	char a = 0xb6;
+//	short b = 0xb600;
+//	int c = 0xb6000000;
+//	if (a == 0xb6)
+//		printf("a");
+//	if (b == 0xb600)
+//		printf("b");
+//	if (c == 0xb6000000)
+//		printf("c");
+//	return 0;
+//}
+
+//int main()
+//{
+//	char c = 1;
+//	printf("%u\n", sizeof(c));
+//	printf("%u\n", sizeof(+c));
+//	printf("%u\n", sizeof(!c));
+//	return 0;
+//}
